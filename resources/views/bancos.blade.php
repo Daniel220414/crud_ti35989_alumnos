f82f5b0fff7cdf206dcf4da0d9c6ae70c86b2902
Luis Daniel <Luis Daniel@DESKTOP-U2I2B8I>
2019-11-13 00:20:28 -0600

    tarea-daniel

--- resources/views/bancos.blade.php
+++ resources/views/bancos.blade.php
@@ -6,12 +6,43 @@
 
 <div class="row">
     <div class="col-md-3">
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">@</span>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-user-o" aria-hidden="true"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
         </div>
-        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
     </div>
+ </div>
+
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-mobile" aria-hidden="true"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Cellphone" aria-label="Cellphone" aria-describedby="basic-addon1">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-unlock" aria-hidden="true"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Code" aria-label="Code" aria-describedby="basic-addon1">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-floppy-o" aria-hidden="true"></i></span>
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
     </div>
 </div>
 
