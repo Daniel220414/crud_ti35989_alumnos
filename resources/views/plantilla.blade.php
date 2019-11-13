e0e6743495dbc23627671cbc2b44f2e15c801cd1
Luis Daniel <Luis Daniel@DESKTOP-U2I2B8I>
2019-11-13 00:23:19 -0600

    tarea-daniel

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
-      <p>Hola mundo Luis Daniel</p>
+      <p>Hola mundo Luis Daniel123</p>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
